826b967835f5b54cd0077bb779e66830885b6d54
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-20 12:59:02 +0800

    feat: Add More Buttons

--- src/components/CustomButtons.tsx
+++ src/components/CustomButtons.tsx
@@ -5,11 +5,20 @@ const CustomButtons = () => {
     <div className="container">
       <h1 className="text-4xl font-bold">Buttons</h1>
 
-      <Button onClick={() => alert("clicked")} className="bg-red-500">
-        Hello
+      <Button onClick={() => alert("clicked primary")} className="bg-red-500">
+        Primary
       </Button>
-      <Button onClick={() => alert("clicked")}>
-        Red
+
+      <Button onClick={() => alert("clicked secondary")} className="bg-red-500">
+        Secondary
+      </Button>
+
+      <Button onClick={() => alert("clicked outlined")} className="bg-red-500">
+        Outlined
+      </Button>
+
+      <Button onClick={() => alert("clicked danger")} className="bg-red-500">
+        Danger
       </Button>
     </div>
   )
